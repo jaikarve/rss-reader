675d99c44c9b34b15287ac19540b5729ed018f00
Jai Karve 2 <jai.karve@gmail.com>
2019-02-05 13:34:38 -0800

    Added events rss feed

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const sdk = require('kinvey-flex-sdk');
 
 const handlers = require('./lib/handlers');
+const eventHandlers = require('./lib/event-handlers');
 
 sdk.service((err, flex) => {
   const data = flex.data;   // gets the FlexData object from the service
 
   handlers.initHandler(flex.logger);
+  eventHandlers.initHandler(flex.logger);
 
   // set the serviceObject
   const rssNews = data.serviceObject('rss-news');
+  const rssEvents = data.serviceObject('rss-events');
+
   // wire up the event that we want to process
   rssNews.onGetAll(handlers.listHandler);
   rssNews.onGetCount(handlers.countHandler);
   rssNews.onGetById(handlers.fetchHandler);
+
+  rssEvents.onGetAll(eventHandlers.listHandler);
+  rssEvents.onGetCount(eventHandlers.countHandler);
 });
